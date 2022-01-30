9c9408432e7f2d5f7d9c5842691daf281bc1d7ad
Nuwan Upekshana <nuwan.upekshana@gmail.com>
2022-01-30 14:03:24 +1100

    product endpoint

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { APIPayload } from 'src/interface';
 import { AppService } from '../services';
 
+@ApiTags('Product')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const payload = this.appService.getHello();
+    const apiPayload: APIPayload = {
+      message: 'productO API Base Endpoint',
+      payload,
+    };
+    return apiPayload;
   }
 }
